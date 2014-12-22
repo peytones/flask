5eb4af15339fd71a6af8d98a4d423edc07a2570e
peytones <peytonsarmiento@gmail.com>
2014-12-22 01:07:44 -0600

    Adding a database to keep track of entered names Database not working

--- forms.py
+++ forms.py
@@ -1,4 +1,5 @@
 import os
+{% import 'database.py' %}
 from flask.ext.bootstrap import Bootstrap
 from flask import Flask, render_template, session, redirect, url_for, flash
 from flask.ext.wtf import Form
@@ -33,12 +34,17 @@ class User(db.Model):
 def index():
 	form = NameForm()
 	if form.validate_on_submit():
-		old_name=session.get('name')
-		if old_name is not None and old_name !=form.name.data:
-			flash('You changed your name!')
+		user=User.query.filter_by(username=form.name.data).first()
+		if user is None:
+			user= User(username=form.name.data)
+			db.session.add(user)	
+			session['known']=False
+		else:
+			session['known']=True
 		session['name']=form.name.data
+		form.name.data=''
 		return redirect(url_for('index'))
-	return render_template('form.html',form=form,name=session.get('name'))
+	return render_template('form.html',form=form,name=session.get('name'),known=session.get('known',False))
 bootstrap=Bootstrap(app)
 if __name__ == "__main__":
 	app.run(debug=True)
