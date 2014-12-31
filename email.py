2c4f5c906696e5383b922317dbea83c613cc7a36
peytones <peytonsarmiento@gmail.com>
2014-12-31 02:14:30 -0600

    chapter six covers email

---
+++ email.py
@@ -0,0 +1,8 @@
+import os
+from flask.ext.mail import Mail
+mail=Mail(app)
+app.config['MAIL_SERVER'] = 'smtp.googlemail.com'
+app.config['MAIL_PORT'] = 587
+app.config['MAIL_USE_TLS'] = True
+app.config['MAIL_USERNAME'] = os.environ.get('MAIL_USERNAME')
+app.config['MAIL_PASSWORD'] = os.environ.get('MAIL_PASSWORD')
